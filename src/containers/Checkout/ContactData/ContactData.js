d96f1eacca2e1982aaac5a91d812455a696b8827
YukiUmetsu <yuuki.umetsu@gmail.com>
2020-02-18 17:59:27 -0600

    added Input UI component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,12 +3,13 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import orderAxios from "../../../axios/axios-orders";
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
 
     constructor(props){
         super(props);
-        this.state = {
+        this.state ={
             name: '',
             email: '',
             address: {
@@ -52,10 +53,10 @@ class ContactData extends Component {
     render(){
         let form = (
             <form action="">
-                <input className={classes.Input} type="text" name="name" placeholder="your name"/>
-                <input className={classes.Input} type="text" name="email" placeholder="your email"/>
-                <input className={classes.Input} type="text" name="street" placeholder="your street"/>
-                <input className={classes.Input} type="text" name="zipcode" placeholder="your zipcode"/>
+                <Input inputtype="input" type="text" name="name" placeholder="your name"/>
+                <Input inputtype="input" type="text" name="email" placeholder="your email"/>
+                <Input inputtype="input" type="text" name="street" placeholder="your street"/>
+                <Input inputtype="input" type="text" name="zipcode" placeholder="your zipcode"/>
                 <Button btnType="Success" clicked={this.orderHandler}>Confirm</Button>
             </form>);
 
