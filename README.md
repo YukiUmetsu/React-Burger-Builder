09e5c6c49debb6ec079205566fcc8a0a172987f4
YukiUmetsu <yuuki.umetsu@gmail.com>
2020-02-21 23:41:26 -0600

    added frontend gaurd to routes

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## .env needed 
+REACT_APP_GOOGLE_API_KEY=XXXXXX
+REACT_APP_CRYPTO_SECRET=XXXXXX
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,7 @@ export const authStart = () => {
 };
 
 export const logOut = () => {
+    removeFromLocalStorage(['token', 'expirationTime', 'userId']);
     return {
         type: actionTypes.AUTH_LOGOUT,
     };
@@ -43,6 +44,20 @@ export const setAuthRedirectPath = (path) => {
     }
 };
 
+const storeInLocalStorage = (dataObj) => {
+    let keys = Object.keys(dataObj);
+    keys.map(key => {
+        let value = dataObj[key];
+        return localStorage.setItem(key, value);
+    });
+};
+
+const removeFromLocalStorage = (keys) => {
+    keys.map(key => {
+        return localStorage.removeItem(key);
+    });
+};
+
 export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
@@ -55,14 +70,35 @@ export const auth = (email, password, isSignUp) => {
         if(!isSignUp){
             postURL = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='+process.env.REACT_APP_GOOGLE_API_KEY;
         }
-        console.log(postURL);
         axios.post(postURL, authData)
             .then((response) => {
+                const expirationDate = new Date((new Date()).getTime() + (response.data.expiresIn * 1000));
+                storeInLocalStorage({
+                    token: response.data.idToken,
+                    expirationDate: expirationDate,
+                    userId: response.data.localId,
+                });
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn))
             })
             .catch((err)=>{
                 dispatch(authFail(err.response.data.error));
             })
+    }
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        const expirationDate = new Date(localStorage.getItem('expirationDate'));
+        const userId = localStorage.getItem('userId');
+
+        if(token && expirationDate > new Date()){
+            dispatch(authSuccess(token, userId));
+            let diffTime = (expirationDate.getTime() - new Date().getTime())/1000;
+            dispatch(checkAuthTimeout(diffTime));
+        } else {
+            dispatch(logOut());
+        }
     }
 };
